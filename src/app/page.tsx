8b5af2990817d2b462625a34ec159c3614310e16
yxiuhli <yxiuhli@gmail.com>
2024-10-03 14:24:43 +0700

    dp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ const HomePage = async () => {
         </Suspense>
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1 className="text-2xl">New Products</h1>
+        <h1 className="text-2xl">New products</h1>
         <Suspense fallback={<Skeleton />}>
           <ProductList
             categoryId={process.env.FEATURED_PRODUCTS_NEW_CATEGORY_ID!}
