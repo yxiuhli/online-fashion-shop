8725cb1685d524e47725d37a986d986ffcb5eebb
anduon <dgngcan@gmail.com>
2024-11-13 21:29:59 +0700

    fix error vs cloudinary

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-            protocol: "https",
-            hostname: "images.pexels.com",
-            },
-            {
-            protocol: "https",
-            hostname: "static.wixstatic.com",
-            },
-            {
-            protocol: "https",
-            hostname: "people.pic1.co",
-            },
-            {
-            protocol: "https",
-            hostname: "app-uploads-cdn.fera.ai",
-            },
-            {
-            protocol: "https",
-            hostname: "lh3.googleusercontent.com",
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.wixstatic.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'people.pic1.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'app-uploads-cdn.fera.ai',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
