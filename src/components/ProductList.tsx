09a000e8e08749593c2470262c14eb7370f155ec
anduon <dgngcan@gmail.com>
2024-11-25 10:04:01 +0700

    add pagination

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -3,7 +3,7 @@ import { products } from '@wix/stores';
 import Image from 'next/image';
 import Link from 'next/link';
 import DOMPurify from 'isomorphic-dompurify';
-// import Pagination from "./Pagination";
+import Pagination from './Pagination';
 
 const PRODUCT_PER_PAGE = 8;
 
@@ -121,13 +121,13 @@ const ProductList = async ({
           </button>
         </Link>
       ))}
-      {/* {searchParams?.cat || searchParams?.name ? (
+      {searchParams?.cat || searchParams?.name ? (
         <Pagination
           currentPage={res.currentPage || 0}
           hasPrev={res.hasPrev()}
           hasNext={res.hasNext()}
         />
-      ) : null} */}
+      ) : null}
     </div>
   );
 };
